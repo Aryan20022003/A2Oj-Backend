ba449e78646e184e8ca50231eee463c6fa5a8438
lemontreejs13 <secretstuff130@gmail.com>
2022-01-22 19:15:33 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import SectionTitle from "./Components/SectionTitle";
 
 
 function App() {
-  window.particlesJS.load('particles', '/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded')
-  })
+  window.particlesJS.load('particles', '/particlesjs-config.json')
 
   return (
- <div className="relative bg-black">
+    <div className="relative bg-black">
       <div className="fixed top-0 left-0">
         <div id="particles" className="w-screen h-screen" />
       </div>
+
       <div className="App bg-black min-h-screen">
         <SectionTitle title="Roadmaps" />
       </div>
+
       <section id="sponsors">
         <Container>
           <Sponsors />
@@ -31,7 +31,7 @@ function App() {
         </Container>
       </section>
     </div>
-    
+
   );
 }
 
