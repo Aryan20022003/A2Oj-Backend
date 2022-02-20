c41ae88c9077c76caf1e66a16f4d377830194669
Atithi Kumar Singh <coder_ravan@pop-os.localdomain>
2022-02-20 15:39:26 +0530

    changed contact page

--- src/Components/FAQs/Faq.js
+++ src/Components/FAQs/Faq.js
@@ -4,7 +4,7 @@ import FaqComp from './FaqComp'
 
 function Faq() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center my-14">
       <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
       <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
       <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
