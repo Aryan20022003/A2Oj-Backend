7ec937449f6236712b26810bd7efa0980f71ee84
Atithi Kumar Singh <coder_ravan@pop-os.localdomain>
2022-02-20 15:22:23 +0530

    added Faq

--- src/Components/FAQs/FaqComp.js
+++ src/Components/FAQs/FaqComp.js
@@ -1,31 +1,55 @@
-import React from 'react';
-import './FaqComp.css';
+import React from "react";
+import "./FaqComp.css";
 function FaqComp(props) {
+	const [active, setActive] = React.useState(false);
 
-  const[active,setActive]=React.useState(false);
-
-  return (
-    <div
-      className="m-4 p-2 cursor-pointer ease-in rounded-2xl shadow-2xl hover:scale-110  duration-200 bg-white"
-      onClick={() => {
-        setActive(!active);
-        console.log(active);
-      }}
-    >
-      <div>
-        <span class="flex   mx-8 my-2 items-center w-full">
-          <span class="animate-ping absolute inline-flex w-8 h-8  rounded-full bg-sky-400 opacity-75"></span>
-          <span class="relative flex justify-center h-8 w-8 rounded-full items-center bg-sky-500 ">
-            {props.sno}
-          </span>
-          <div className="mx-8 text-xl font-semibold">{props.ques}</div>
-        </span>
-      </div>
-      <div className={`${active ? "hidden" : null} ease-in duration-300 `}>
-        <div className="">{props.ans}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="m-4 p-2 cursor-pointer ease-in rounded-2xl shadow-2xl hover:scale-110 w-4/5  duration-200 bg-white"
+			onClick={() => {
+				setActive(!active);
+				console.log(active);
+			}}
+		>
+			<div>
+				<span class="flex  px-4 lg:px-8 my-2 items-center w-full">
+					<span class=" animate-ping absolute inline-flex w-6 h-6 sm:w-8 sm:h-8   rounded-full bg-sky-400 opacity-75"></span>
+					<span class="flex-shrink-0 relative flex justify-center h-6 w-6 sm:w-8 sm:h-8  rounded-full items-center bg-sky-500 ">
+						{props.sno}
+					</span>
+					<div className="ml-4 text-sm lg:text-xl font-semibold">
+						{props.ques}
+					</div>
+					<div className="ml-auto">
+						<svg
+							className={`h-6 w-6 ${
+								active
+									? ""
+									: "transform rotate-180"
+							}  ease-in-out duration-100`}
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M19 9l-7 7-7-7"
+							/>
+						</svg>
+					</div>
+				</span>
+			</div>
+			<div
+				className={`${
+					active ? "hidden" : null
+				} ease-in duration-300 `}
+			>
+				<div className="">{props.ans}</div>
+			</div>
+		</div>
+	);
 }
 
 export default FaqComp;
