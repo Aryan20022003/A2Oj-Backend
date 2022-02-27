7214acc133bcd2444ec053f22a51bebdf1834378
Tanmay Shree <tanmay.25ts@gmail.com>
2022-02-27 22:04:23 +0530

    Page look Modified

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,25 +7,108 @@ const Navbar = () => {
 
     const [menu, setMenu] = useState(false);
 
-
     return (
+        <div className="navpage">
+            <nav className="navbox">
+                <img src={logo} alt="" />
+                <div className="links-list">
 
-        <nav>
-            <div className="nav_left">
-                <img className='nav_logo' src={logo} alt="" />
+                    <li className="list-items">About</li>
+                    <li className="list-items">Prizes</li>
+                    <li className="list-items">Schedule</li>
+                    <li className="list-items">Our Team</li>
+                    <li className="list-items">Testimonials</li>
+                    <li className="list-items">Contact Us</li>
+                </div>
+            </nav>
+
+            <nav className="mobile-nav">
+                <img src={logo} alt="Logo" />
+                <i
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className={!menu ? "fas fa-bars" : "far fa-times-circle text-3xl"}
+                />
+            </nav>
+
+            <div className={!menu ? "mobile-list" : "mobile-list is-active"}>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    About
+                </li>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    Prizes
+                </li>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    Schedule
+                </li>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    Mentors
+                </li>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    Sponsors
+                </li>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    FAQs
+                </li>
+                <li
+                    onClick={() => {
+                        setMenu(!menu);
+                    }}
+                    className="list-items"
+                >
+                    Contact Us
+                </li>
             </div>
-            <i className="nav_icon fa-solid fa-bars" />
-            <ul className="nav_right nav-active">
-                <li className='nav_item'>About</li>
-                <li className='nav_item'>Prizes</li>
-                <li className='nav_item'>Schedule</li>
-                <li className='nav_item'>Mentors</li>
-                <li className='nav_item'>Sponsors</li>
-                <li className='nav_item'>FAQs</li>
-                <li className='nav_item'>Contact Us</li>
-            </ul>
-        </nav>
+        </div>
     );
+    // return (
+    //     <nav>
+    //         <div className="nav_left">
+    //             <img className='nav_logo' src={logo} alt="" />
+    //         </div>
+    //         <i className="nav_icon fa-solid fa-bars" />
+    //         <ul className="nav_right nav-active">
+    //             <li className='nav_item'>About</li>
+    //             <li className='nav_item'>Prizes</li>
+    //             <li className='nav_item'>Schedule</li>
+    //             <li className='nav_item'>Mentors</li>
+    //             <li className='nav_item'>Sponsors</li>
+    //             <li className='nav_item'>FAQs</li>
+    //             <li className='nav_item'>Contact Us</li>
+    //         </ul>
+    //     </nav>
+    // );
 }
 
 export default Navbar;
--- src/Components/Sponsors/index.js
+++ src/Components/Sponsors/index.js
@@ -1,33 +1,73 @@
 import SectionTitle from "../SectionTitle"
-
-export default function Sponsors() {
-  const sponsorCards = Array(9).fill(
-    <div className="rounded-lg overflow-hidden">
-      <h3 className="w-full bg-red-500 text-center p-1 text-gray-50 text-lg font-semibold">
-        Rebook
-      </h3>
-      <div className="w-full flex justify-center relative">
-        <img src="/Images/sponsors/mcdonalds-logo.png" alt="" className="relative z-10 py-5" />
-        <div className="absolute flex w-full h-full flex-col">
-          <div className="flex-grow bg-gradient-to-b from-white to-gray-100" />
-          <div className="-mt-px w-full h-32 bg-gradient-to-b from-gray-100" />
-        </div>
-      </div>
-    </div>
-  ).map((card, i) => (
-    <div key={ i }>
-      { card }
-    </div>
-  ))
+import "./sponsors.css";
+import logo from "./../../assets/logo/logo-light.png";
+import { SponsorsData } from "./../SponsorsData";
+const Sponsors = () => {
   return (
-    <>
+    <div className="sponsors-page">
       <SectionTitle title="Sponsors" />
-      <p className="mt-6 capitalize text-xl text-center sm:text-2xl lg:text-left text-white tracking-wider font-semibold">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac euismod neque. Mauris arcu arcu, lobortis <span className="text-red-600">nec maximus egestas, maximus eget lacus.</span> la facilisi.
-      </p>
-      <div className="mt-12 sm:mt-16 md:mt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-14">
-        { sponsorCards }
+      <div class="container">
+
+        {/* <div class="card">
+          <div class="box">
+            <div class="content">
+              <img src={logo} alt="" />
+              <h3>Google</h3>
+            </div>
+          </div>
+        </div> */}
+        
+        {
+        SponsorsData.map((Sponsor) => {
+          return(
+            <div className= "card" key={Sponsor.index} >
+              <div className="box">
+                <div className="content">
+                  <img src={Sponsor.img} alt="" />
+                  <h3>{Sponsor.name}</h3>
+                </div>
+              </div>
+            </div>
+          )
+        }) 
+      }
+
       </div>
-    </>
-  )
+    </div>
+  );
 }
+
+export default Sponsors;
+
+// export default function Sponsors() {
+//   const sponsorCards = Array(9).fill(
+//     <div className="rounded-lg overflow-hidden">
+//       <h3 className="w-full bg-red-500 text-center p-1 text-gray-50 text-lg font-semibold">
+//         Rebook
+//       </h3>
+//       <div className="w-full flex justify-center relative">
+//         <img src="/Images/sponsors/mcdonalds-logo.png" alt="" className="relative z-10 py-5" />
+//         <div className="absolute flex w-full h-full flex-col">
+//           <div className="flex-grow bg-gradient-to-b from-white to-gray-100" />
+//           <div className="-mt-px w-full h-32 bg-gradient-to-b from-gray-100" />
+//         </div>
+//       </div>
+//     </div>
+//   ).map((card, i) => (
+//     <div key={ i }>
+//       { card }
+//     </div>
+//   ))
+//   return (
+//     <>
+//       <SectionTitle title="Sponsors" />
+//       <p className="mt-6 capitalize text-xl text-center sm:text-2xl lg:text-left text-white tracking-wider font-semibold">
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac euismod neque. Mauris arcu arcu, lobortis <span className="text-red-600">nec maximus egestas, maximus eget lacus.</span> la facilisi.
+//       </p>
+//       <div className="mt-12 sm:mt-16 md:mt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-14">
+//         { sponsorCards }
+//       </div>
+//     </>
+//   )
+// }
+
