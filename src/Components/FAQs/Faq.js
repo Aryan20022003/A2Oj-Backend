8f53916edf271e7e555ec5801fe8b323957f1931
Ankit Gautam <a53gau@gmail.com>
2022-02-27 22:06:14 +0530

    some fixes

--- src/Components/FAQs/Faq.js
+++ src/Components/FAQs/Faq.js
@@ -7,12 +7,12 @@ function Faq() {
   return (
     <>
       <SectionTitle title={'FAQs'} />
-      <div className="flex flex-col justify-center items-center my-14">
+      <div className="flex flex-col justify-center items-center my-10">
         <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
-        <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
-        <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
-        <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
-        <FaqComp sno="1" ques="Why this kolaveri di?" ans="Jai shree Ram" />
+        <FaqComp sno="2" ques="Why this kolaveri di?" ans="Jai shree Ram" />
+        <FaqComp sno="3" ques="Why this kolaveri di?" ans="Jai shree Ram" />
+        <FaqComp sno="4" ques="Why this kolaveri di?" ans="Jai shree Ram" />
+        <FaqComp sno="5" ques="Why this kolaveri di?" ans="Jai shree Ram" />
       </div>
     </>
   );
