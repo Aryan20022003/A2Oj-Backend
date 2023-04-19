18a0f2398bba46cd9db121839dbd3305af7b01db
chetansaini <chetansaini1241@gmail.com>
2023-04-19 14:59:50 +0530

    DCC Logo and Name

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ export const Footer = () => {
                 </div>
                 <div class="secondContainer">
                     <div class="location">
-                        <h3>DEVELOPERS AND CODERS CLUB NITA</h3>
+                        <h3>DEVELOPERS & CODERS CLUB NITA</h3>
                         <p>
                             National Institute of Technology, Agartala,
                             <br />
