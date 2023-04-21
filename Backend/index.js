9795bdfd0dfe3e6423ff34191b75282922765985
SinghAtithi <mails4rockstararyan@gmail.com>
2023-04-22 02:35:49 +0530

    Fixed Editor

--- Backend/index.js
+++ Backend/index.js
@@ -1,45 +1,34 @@
 const express = require('express');
-const { exec } = require('child_process');
 const app = express();
+const axios = require("axios");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/', async (req, res) => {
       const { code, input } = req.body;
-
-      // Write the code and input to files
-      const codeFilePath = 'code.cpp';
-      const inputFilePath = 'input.txt';
-      require('fs').writeFileSync(codeFilePath, code);
-      require('fs').writeFileSync(inputFilePath, input);
-
-      try {
-            // Compile and run the C++ code with a 3-second timeout
-            const { stdout, stderr } = await execPromise(`timeout 3s g++ ${codeFilePath} -o code && timeout 3s ./code < ${inputFilePath}`);
-            console.log(stdout);
-            console.error(stderr);
-            res.send(stdout);
-      } catch (err) {
-            console.error(err);
-            if (err.message.includes('exceeded')) {
-                  return res.status(522).send('Time limit exceeded');
-            } else {
-                  return res.status(520).send(`An error occurred while compiling the code. ${err.message}`);
+      console.log(code); console.log(input);
+      var program = {
+            script: code,
+            language: "cpp17",
+            versionIndex: "0",
+            stdin: input,
+            clientId: "857e060c5620048e16181c905d8a0688",
+            clientSecret:
+                  "3a9af02cba6007f067445d80c458c4a90acd9dcfc740079fc3a20e204961b1fe",
+      };
+      axios
+            .post(
+                  "https://api.jdoodle.com/v1/execute",
+                  program
+            ).then((result) => {
+                  console.log(result.data.output);
+                  res.send(result.data.output);
             }
-      }
-});
-
-function execPromise(command) {
-      return new Promise((resolve, reject) => {
-            exec(command, (err, stdout, stderr) => {
-                  if (err) {
-                        reject(err);
-                  } else {
-                        resolve({ stdout, stderr });
-                  }
+            ).catch((err) => {
+                  console.log(err.output);
+                  res.send(err.output);
             });
-      });
-}
+});
 
 app.listen(3579, () => console.log('Server running on port 3579'));
